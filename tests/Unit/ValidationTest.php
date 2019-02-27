989ee518e19a358e1148c93158dd747fb27d795a
Choraimy Kroonstuiver <choraimy@23g.nl>
2019-02-27 20:36:39 +0100

    Add Laravel 5.8 support

--- tests/Unit/ValidationTest.php
+++ tests/Unit/ValidationTest.php
@@ -33,7 +33,7 @@ class ValidationTest extends TestCase
      *
      * @return void
      */
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
         $this->factory = $this->app->make('validator');
