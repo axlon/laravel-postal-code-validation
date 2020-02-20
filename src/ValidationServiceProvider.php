5f154aeda9a70657c132e82179fcef2ebf3f4bc8
Choraimy Kroonstuiver <choraimy@23g.nl>
2020-02-20 22:47:43 +0100

    🔧 Register rules if validator is already resolved

--- src/ValidationServiceProvider.php
+++ src/ValidationServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Axlon\PostalCodeValidation;
 
+use Axlon\PostalCodeValidation\Extensions\PostalCodeFor;
 use Illuminate\Contracts\Validation\Factory;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,22 +13,37 @@ class ValidationServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
-        $this->app->singleton('Axlon\PostalCodeValidation\Validator');
-        $this->app->singleton('Axlon\PostalCodeValidation\Extensions\PostalCodeFor');
+        if ($this->app->resolved('validator')) {
+            $this->registerRules($this->app['validator']);
+        } else {
+            $this->app->resolving('validator', function (Factory $validator) {
+                $this->registerRules($validator);
+            });
+        }
 
-        $this->app->afterResolving('validator', function (Factory $factory) {
-            $factory->extend('postal_code', 'Axlon\PostalCodeValidation\Extensions\PostalCode@validate');
-            $factory->replacer('postal_code', 'Axlon\PostalCodeValidation\Extensions\PostalCode@replace');
+        $this->app->singleton(PostalCodeFor::class);
+        $this->app->singleton(Validator::class);
+    }
 
-            if (method_exists($factory, 'extendDependent')) {
-                $factory->extendDependent('postal_code_for', 'Axlon\PostalCodeValidation\Extensions\PostalCodeFor@validate');
-                $factory->replacer('postal_code_for', 'Axlon\PostalCodeValidation\Extensions\PostalCodeFor@replace');
-            } else {
-                $factory->extend('postal_code_for', 'Axlon\PostalCodeValidation\Extensions\PostalCodeFor@validate');
-                $factory->replacer('postal_code_for', 'Axlon\PostalCodeValidation\Extensions\PostalCodeFor@replace');
-            }
-        });
+    /**
+     * Register the postal code validation rules with the validator.
+     *
+     * @param \Illuminate\Contracts\Validation\Factory $validator
+     * @return void
+     */
+    public function registerRules(Factory $validator): void
+    {
+        $validator->extend('postal_code', 'Axlon\PostalCodeValidation\Extensions\PostalCode@validate');
+        $validator->replacer('postal_code', 'Axlon\PostalCodeValidation\Extensions\PostalCode@replace');
+
+        if (method_exists($validator, 'extendDependent')) {
+            $validator->extendDependent('postal_code_for', 'Axlon\PostalCodeValidation\Extensions\PostalCodeFor@validate');
+            $validator->replacer('postal_code_for', 'Axlon\PostalCodeValidation\Extensions\PostalCodeFor@replace');
+        } else {
+            $validator->extend('postal_code_for', 'Axlon\PostalCodeValidation\Extensions\PostalCodeFor@validate');
+            $validator->replacer('postal_code_for', 'Axlon\PostalCodeValidation\Extensions\PostalCodeFor@replace');
+        }
     }
 }
