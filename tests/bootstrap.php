2022d87d4e0649a4e1ccf356e7c27ddc88d7b37b
Choraimy Kroonstuiver <choraimy@live.nl>
2020-02-20 21:48:29 +0100

    🔧 Remove PHPUnit 5 and 6 from builds (#12)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,16 +2,6 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-# PHPUnit wasn't namespaced before PHPUnit 6
-if (!class_exists('\PHPUnit\Framework\Constraint\Constraint')) {
-    class_alias('\PHPUnit_Framework_Constraint', '\PHPUnit\Framework\Constraint\Constraint');
-}
-
-# PHPUnit wasn't namespaced before PHPUnit 6
-if (!class_exists('\PHPUnit\Framework\TestCase')) {
-    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
-}
-
 # The array loader was introduced in Laravel 5.2
 if (!class_exists('\Illuminate\Contracts\Translation\Loader\ArrayLoader')) {
 
